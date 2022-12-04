fd4cdf3b0ada86ff736462c1a83e07ba634bbbde
karnjj <karnjj@hotmail.com>
2022-12-05 01:11:39 +0700

    feat: init item by scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_04_175529) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_04_181023) do
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.string "category"
+    t.boolean "enable"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "name"
