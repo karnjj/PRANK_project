dd4e5f3c15a1311422c920f35eb6c3b06ed81b27
karnjj <karnjj@hotmail.com>
2022-12-14 21:02:59 +0700

    implement my-market page

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,23 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "@hotwired/turbo-rails";
+import "controllers";
 
-import "jquery"
-import "jquery_ujs"
-import * as jq from 'jquery';
+import "jquery";
+import "jquery_ujs";
+import * as jq from "jquery";
 window.importmapScriptsLoaded = true;
+
+var table = $("#my_market_table").DataTable({
+  order: [],
+  columnDefs: [{ orderable: false, targets: [0, -1] }],
+  oLanguage: {
+    sSearch: "Search By Category:",
+  },
+});
+
+$(".dataTables_filter input")
+  .unbind()
+  .keyup(function (e) {
+    var value = $(this).val();
+    table.column(2).search(value).draw();
+  });
