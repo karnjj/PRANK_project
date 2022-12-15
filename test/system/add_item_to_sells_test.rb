4e12664560fa6baf1caa846c6b9d86dc73b96494
Karn Jaroenporn <karnjj@hotmail.com>
2022-12-16 00:21:35 +0700

    test: test add item page (#22)

--- test/system/add_item_to_sells_test.rb
+++ test/system/add_item_to_sells_test.rb
@@ -1,17 +1,18 @@
 require "application_system_test_case"
 
-class EditItemInventoriesTest < ApplicationSystemTestCase
+class AddItemToSellsTest < ApplicationSystemTestCase
   # test "visiting the index" do
-  #   visit edit_item_inventories_url
+  #   visit add_item_to_sells_url
   #
-  #   assert_selector "h1", text: "EditItemInventory"
+  #   assert_selector "h1", text: "AddItemToSell"
   # end
 
   setup do
-    @market = markets(:one)
+    market = markets(:one)
+    market.destroy
   end
 
-  test "visit edit item inventory by seller" do
+  test "visit add item to sell by seller" do
     # login as seller
     user = users(:two)
 
@@ -21,11 +22,11 @@ class EditItemInventoriesTest < ApplicationSystemTestCase
     click_on "Login"
     assert_text "main"
 
-    visit edit_item_inventory_url(@market)
-    assert_text "Edit Item Inventory"
+    visit add_item_to_sell_url
+    assert_text "Add Item to Sell"
   end
 
-  test "visit edit item inventory by buyer should not allow" do
+  test "visit add item to sell by buyer should not allow" do
     # login as buyer
     user = users(:three)
 
@@ -35,11 +36,11 @@ class EditItemInventoriesTest < ApplicationSystemTestCase
     click_on "Login"
     assert_text "main"
 
-    visit edit_item_inventory_url(@market)
+    visit add_item_to_sell_url
     assert_text "ไม่มีสิทธิเข้าถึง"
   end
 
-  test "visit edit item inventory by admin" do
+  test "visit add item to sell by admin" do
     # login as admin
     user = users(:one)
 
@@ -49,11 +50,11 @@ class EditItemInventoriesTest < ApplicationSystemTestCase
     click_on "Login"
     assert_text "main"
 
-    visit edit_item_inventory_url(@market)
-    assert_text "Edit Item Inventory"
+    visit add_item_to_sell_url
+    assert_text "Add Item to Sell"
   end
 
-  test "visit edit item inventory & can edit item inventory" do
+  test "visit add item to sell & can add item to sell" do
     # login as seller
     user = users(:two)
 
@@ -63,20 +64,26 @@ class EditItemInventoriesTest < ApplicationSystemTestCase
     click_on "Login"
     assert_text "main"
 
-    visit edit_item_inventory_url(@market)
-    assert_text "Edit Item Inventory"
+    visit my_inventory_url
+    assert_no_selector "td", text: "item1"
 
-    assert_selector "label", text: "Price"
-    assert_selector "label", text: "Stock"
+    visit add_item_to_sell_url
+    assert_text "Add Item to Sell"
+
+    item = Item.find_by(name: "item1")
+
+    choose("item_id_#{item.id}")
+
     fill_in "price", with: 1987
     fill_in "stock", with: 3987
 
-    click_on "Save"
+    click_on "Add"
 
     # return to my inventory
     assert_text "My Inventory"
 
-    # check if item inventory is updated
+    # check if item inventory is added
+    assert_selector "td", text: "item1"
     assert_text "1987"
     assert_text "3987"
   end
